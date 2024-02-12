f01e7bdf188dba363b83624fc5d4451298af1e89
baktybekovich- <kydyrmysheva2005@gmail.com>
2024-02-12 19:46:53 +0600

    db settings

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ComtehnoPpsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class IndexController extends AbstractController
 {
+    public function __construct(private ComtehnoPpsRepository $ppsRepository)
+    {
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller test! sdfsdf',
-            'path' => 'src/Controller/IndexController.php',
+            $this->ppsRepository->findAll()
         ]);
     }
 }
