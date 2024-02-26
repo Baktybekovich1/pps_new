35d1e6de814d36331ac6fce7fdc8a084dfcc7611
baktybekovich1 <kydyrmysheva2005@gmail.com>
2024-02-26 10:07:09 +0600

    user fill a

---
+++ src/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\DataFixtures\IntitutesFixtures;
+use App\Repository\AcademicDegreeRepository;
+use App\Repository\AcademicRankRepository;
+use App\Repository\InstitutionsRepository;
+use App\Repository\PositionsRepository;
+use App\Repository\StateAwardsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    public function __construct(
+        private InstitutionsRepository   $institutionsRepository,
+        private PositionsRepository      $positionsRepository,
+        private AcademicDegreeRepository $degreeRepository,
+        private AcademicRankRepository   $rankRepository,
+        private StateAwardsRepository    $stateAwardsRepository
+    )
+    {
+    }
+
+    #[Route('/user/fill', name: 'app_user')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'institutes' => $this->institutionsRepository->findAll(),
+            'positons' => $this->positionsRepository->findAll(),
+            'degree' => $this->degreeRepository->findAll(),
+            'rank' => $this->rankRepository->findAll(),
+            'state_awards' => $this->stateAwardsRepository->findAll()
+        ]);
+    }
+}
