d44c023066c324ee1e62b1d9aaad1b162a3a8c5a
baktybekovich1 <kydyrmysheva2005@gmail.com>
2024-04-30 10:41:30 +0500

    Positions new

--- src/Controller/user/UserInfoController.php
+++ src/Controller/user/UserInfoController.php
@@ -23,97 +23,77 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserInfoController extends AbstractController
 {
-    public function __construct(
-        private readonly InstitutionsRepository               $institutionsRepository,
-        private readonly PositionsRepository                  $positionsRepository,
-        private readonly UserInfoRepository                   $userInfoRepository,
-        private readonly UserRepository                       $userRepository,
-        private readonly UserPersonalAwardsRepository         $userPersonalAwardsRepository,
-        private readonly UserResearchActivitiesListRepository $userResearchActivitiesListRepository,
-        private readonly UserInnovativeEducationRepository    $userInnovativeEducationRepository,
-        private readonly UserSocialActivitiesRepository       $userSocialActivitiesRepository
-    )
-    {
-    }
-
-    #[Route('/name', name: 'app_user_name')]
-    public function user_name(UserInterface $user): JsonResponse
-    {
-        $user = $this->userRepository->find($user->getUserIdentifier());
-        $userInfo = $this->userInfoRepository->findOneBy(['user' => $user]);
-        $dto = new UserInfoGetDto(
-            $userInfo->getId(),
-            $userInfo->getName(),
-            $userInfo->getInstitutions()->getName(),
-            $userInfo->getPosition(),
-            $userInfo->getRegular(),
-            $userInfo->getEmail()
-        );
-
-        return $this->json([
-            'user' => $dto
-        ]);
-    }
-
-    #[Route('/info', name: 'app_user_info')]
-    public function user_info(): JsonResponse
-    {
-        return $this->json([
-            'institutes' => $this->institutionsRepository->findAll(),
-            'position' => $this->positionsRepository->findAll()
-        ]);
-    }
-
-    #[Route('/us/{id}', name: 'app_user_us')]
-    public function us(Request $request): JsonResponse
-    {
-        return $this->json([
-            'id' => $this->userInfoRepository->findOneBy(['user' => $this->userRepository->find($request->get('id'))])
-        ]);
-    }
-
-    #[Route('/info/add', name: 'app_user_form', methods: ['POST'])]
-    public function user_form_save(UserInterface $user, #[MapRequestPayload] UserInfoDto $dto): JsonResponse
-    {
-        $id = $user->getUserIdentifier();
-        $userInfo = new UserInfo();
-        $userInfo->setName($dto->name);
-        $userInfo->setUser($this->userRepository->find($user->getUserIdentifier()));
-        $userInfo->setInstitutions($this->institutionsRepository->find($this->institutionsRepository->find($dto->institut)));
-        $userInfo->setPosition($dto->position);
-        $userInfo->setRegular($dto->regular);
-        $userInfo->setEmail($dto->email);
-        $this->userInfoRepository->save($userInfo);
-        return $this->json([
-            'name' => $dto->name
-        ]);
-    }
-
-//    #[Route('/info/list/{id}', name: 'app_user_info_list')]
-//    public function user_info_list(Request $request): JsonResponse
+//    public function __construct(
+//        private readonly InstitutionsRepository               $institutionsRepository,
+//        private readonly PositionsRepository                  $positionsRepository,
+//        private readonly UserInfoRepository                   $userInfoRepository,
+//        private readonly UserRepository                       $userRepository,
+//        private readonly UserPersonalAwardsRepository         $userPersonalAwardsRepository,
+//        private readonly UserResearchActivitiesListRepository $userResearchActivitiesListRepository,
+//        private readonly UserInnovativeEducationRepository    $userInnovativeEducationRepository,
+//        private readonly UserSocialActivitiesRepository       $userSocialActivitiesRepository
+//    )
 //    {
-//        $user = $this->userRepository->find($request->get('id'));
-//        $name = $this->userInfoRepository->findOneBy(['user' => $user]);
-//        $personalAwards = $this->userPersonalAwardsRepository->findBy(['user' => $user]);
-//        $researchActivities = $this->userResearchActivitiesListRepository->findBy(['user' => $user]);
-//        $innovative = $this->userInnovativeEducationRepository->findBy(['user' => $user]);
-//        $social = $this->userSocialActivitiesRepository->findBy(['user' => $user]);
-//        $name = $this->userInfoRepository->findOneBy(['user' => $user]);
+//    }
+//
+//    #[Route('/name', name: 'app_user_name')]
+//    public function user_name(UserInterface $user): JsonResponse
+//    {
+//        $user = $this->userRepository->find($user->getUserIdentifier());
+//        $userInfo = $this->userInfoRepository->findOneBy(['user' => $user]);
+//        $dto = new UserInfoGetDto(
+//            $userInfo->getId(),
+//            $userInfo->getName(),
+//            $userInfo->getInstitutions()->getName(),
+//            $userInfo->getPosition(),
+//            $userInfo->getRegular(),
+//            $userInfo->getEmail()
+//        );
 //
 //        return $this->json([
-//            'name' => $name->getName(),
-////            'personalAwards' => $this->userPersonalAwardsRepository->findBy(['user' => $user]),
-//            'researchActivities' => $this->userResearchActivitiesListRepository->findBy(['user' => $user]),
-//            'innovative' => $this->userInnovativeEducationRepository->findBy(['user' => $user]),
-//            'social' => $this->userSocialActivitiesRepository->findBy(['user' => $user])
+//            'user' => $dto
 //        ]);
+//    }
 //
+//    #[Route('/info', name: 'app_user_info')]
+//    public function user_info(): JsonResponse
+//    {
+//        return $this->json([
+//            'institutes' => $this->institutionsRepository->findAll(),
+//            'position' => $this->positionsRepository->findAll()
+//        ]);
+//    }
+//
+//    #[Route('/us/{id}', name: 'app_user_us')]
+//    public function us(Request $request): JsonResponse
+//    {
+//        return $this->json([
+//            'id' => $this->userInfoRepository->findOneBy(['user' => $this->userRepository->find($request->get('id'))])
+//        ]);
+//    }
+//
+//    #[Route('/info/add', name: 'app_user_form', methods: ['POST'])]
+//    public function user_form_save(UserInterface $user, #[MapRequestPayload] UserInfoDto $dto): JsonResponse
+//    {
+//        $id = $user->getUserIdentifier();
+//        $userInfo = new UserInfo();
+//        $userInfo->setName($dto->name);
+//        $userInfo->setUser($this->userRepository->find($user->getUserIdentifier()));
+//        $userInfo->setInstitutions($this->institutionsRepository->find($this->institutionsRepository->find($dto->institut)));
+//        $userInfo->setPosition($dto->position);
+//        $userInfo->setRegular($dto->regular);
+//        $userInfo->setEmail($dto->email);
+//        $this->userInfoRepository->save($userInfo);
+//        return $this->json([
+//            'name' => $dto->name
+//        ]);
 //    }
 
-    #[Route('/id', name: 'app_user_id')]
-    public function id(UserInterface $userInterface): JsonResponse
-    {
-        $user = $this->userRepository->find($userInterface->getUserIdentifier());
-        return $this->json([$user->getId()]);
-    }
+
+//    #[Route('/id', name: 'app_user_id')]
+//    public function id(UserInterface $userInterface): JsonResponse
+//    {
+//        $user = $this->userRepository->find($userInterface->getUserIdentifier());
+//        return $this->json([$user->getId()]);
+//    }
 }
