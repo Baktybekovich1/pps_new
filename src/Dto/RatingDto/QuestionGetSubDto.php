8ec0461f8b29f0473ddd1436ab0e6f61feeeb15b
baktybekovich1 <kydyrmysheva2005@gmail.com>
2024-06-27 10:25:34 +0500

    Question Getw

--- src/Dto/RatingDto/QuestionGetSubDto.php
+++ src/Dto/RatingDto/QuestionGetSubDto.php
@@ -2,10 +2,13 @@
 
 namespace App\Dto\RatingDto;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class QuestionGetSubDto
 {
     public function __construct(
         public int $id,
+        #[Assert\IsNull]
         public string $name,
     )
     {
