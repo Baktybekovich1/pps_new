07189f804bf3f709392a7217bdb9073c08c235d6
baktybekovich1 <kydyrmysheva2005@gmail.com>
2024-07-12 17:54:10 +0500

    Test message to email

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,53 +2,47 @@
 
 namespace App\Controller;
 
-use App\Entity\Awards;
-use App\Entity\ComtehnoPps;
-use App\Repository\AwardsRepository;
-use App\Repository\ComtehnoPpsRepository;
+
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use App\Util\ValidationErrors;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class IndexController extends AbstractController
 {
-    public function __construct()
+    public function __construct(private MailerInterface $mailer)
     {
     }
 
-    #[Route('/pps', name: 'app_index', methods: ['POST'])]
-    public function index(Request $request, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
+    #[Route('/pps', name: 'app_index')]
+    public function index(): JsonResponse
     {
-//        $pps = $serializer->deserialize($request->getContent(), ComtehnoPps::class, "json");
-//
-//        $violations = $validator->validate($pps);
-//
-//        if ($violations->count() > 0) {
-//            return $this->json(ValidationErrors::format($violations));
-//        }
-//
-//        $this->ppsRepository->save($pps);
-//        return new JsonResponse(["SAVED"], Response::HTTP_CREATED);
+        $email = (new Email())
+            ->from('kydyrmysheva2005@gmai.com')
+            ->to('tresunionfun@gmail.com')
+            ->subject('Новый вход на сайт')
+            ->text('Пользователь зашел на сайт.');
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface) {
+            $this->json(["ERROR"]);
+        }
+        return $this->json(['HELLO']);
     }
 
     #[Route('/pps/awards', name: 'app_awards', methods: ['POST'])]
-    public function awards_add(Request $request, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse
+    public function awards_add(): JsonResponse
     {
-//        $awards = $serializer->deserialize($request->getContent(), Awards::class, 'json');
-//        $viola = $validator->validate($awards);
-//        if ($viola->count() > 0) {
-//            return $this->json(ValidationErrors::format($viola));
-//        }
-//        $this->awardsRepository->save($awards);
-//
-//        return new JsonResponse(['AWARDS SAVED SUCCESS'], Response::HTTP_CREATED);
-
+        return $this->json(['HELLO']);
     }
 
 }
