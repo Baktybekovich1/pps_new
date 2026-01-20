6d1073912250f5698a4c2316f944068eec0033f4
baktybekovich1 <kydyrmysheva2005@gmail.com>
2026-01-20 23:14:02 +0600

    Teacher auth test

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Teacher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,21 +13,31 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 class MeController extends AbstractController
 {
 
-
-    #[Route('/api/me', name: 'me', methods: ['GET'])]
-    public function __invoke(#[CurrentUser] ?Teacher $teacher,Request $request): Response
+    #[Route('/api/me')]
+    public function apiMe(Security $security): Response
     {
-        dump(
-            'Authorization header:', $request->headers->get('Authorization'),
+        // Всегда через Security сервис
+        $user = $security->getUser();
 
-            'Teacher:', $teacher,
-            'JWT roles:', $this->getUser()?->getRoles()
-        );
-        if (!$teacher) {
-            return $this->json(['error' => 'Unauthorized'], 401);
-        }
-
-        return $this->json($teacher, 200, [], ['groups' => 'teacher:read']);
+        dump($user);
+        return $this->json($user);
     }
+
+//    #[Route('/api/me', name: 'me', methods: ['GET'])]
+//    public function __invoke(#[CurrentUser] ?Teacher $teacher,Request $request): Response
+//    {
+//        dump(
+//            'Authorization header:', $request->headers->get('Authorization'),
+//
+//            'Teacher:', $teacher,
+//            'JWT roles:', $this->getUser()?->getRoles()
+//        );
+//
+//        if (!$teacher) {
+//            return $this->json(['error' => 'Unauthorized'], 401);
+//        }
+//
+//        return $this->json($teacher, 200, [], ['groups' => 'teacher:read']);
+//    }
 }
 
