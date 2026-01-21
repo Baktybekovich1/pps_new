a2fbc23f06187c93a98eccfdea6f6ccc60353b0f
baktybekovich1 <kydyrmysheva2005@gmail.com>
2026-01-21 14:01:51 +0600

    Teacher auth test

---
+++ src/EventSubscriber/CorsSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CorsSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::RESPONSE => ['onKernelResponse', 0],
+        ];
+    }
+
+    public function onKernelResponse(ResponseEvent $event): void
+    {
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+
+        // Добавляем заголовки только для API запросов
+        if (str_starts_with($request->getPathInfo(), '/api/')) {
+            $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Expose-Headers', 'Authorization');
+        }
+    }
+}
