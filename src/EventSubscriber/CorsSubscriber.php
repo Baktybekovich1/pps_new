d3391fea83b79d2c3767e769c10f6ba304da0111
baktybekovich1 <kydyrmysheva2005@gmail.com>
2026-01-27 17:03:27 +0600

    Teacher and User auth setting

--- src/EventSubscriber/CorsSubscriber.php
+++ src/EventSubscriber/CorsSubscriber.php
@@ -12,22 +12,22 @@ class CorsSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::RESPONSE => ['onKernelResponse', 0],
+//            KernelEvents::RESPONSE => ['onKernelResponse', 0],
         ];
     }
 
-    public function onKernelResponse(ResponseEvent $event): void
-    {
-        $request = $event->getRequest();
-        $response = $event->getResponse();
-
-        // Добавляем заголовки только для API запросов
-        if (str_starts_with($request->getPathInfo(), '/api/')) {
-            $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
-            $response->headers->set('Access-Control-Allow-Credentials', 'true');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-            $response->headers->set('Access-Control-Expose-Headers', 'Authorization');
-        }
-    }
+//    public function onKernelResponse(ResponseEvent $event): void
+//    {
+//        $request = $event->getRequest();
+//        $response = $event->getResponse();
+//
+//        // Добавляем заголовки только для API запросов
+//        if (str_starts_with($request->getPathInfo(), '/api/')) {
+//            $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
+//            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+//            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+//            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//            $response->headers->set('Access-Control-Expose-Headers', 'Authorization');
+//        }
+//    }
 }
