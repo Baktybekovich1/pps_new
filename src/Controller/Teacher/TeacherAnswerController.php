77069d38ae65ba79fbf40090e7ecaf07f67c5d02
baktybekovich1 <kydyrmysheva2005@gmail.com>
2026-03-03 16:03:54 +0600

    Private office edit and delete awards

--- src/Controller/Teacher/TeacherAnswerController.php
+++ src/Controller/Teacher/TeacherAnswerController.php
@@ -35,6 +35,7 @@ class TeacherAnswerController extends AbstractController
     #[Route(path: '/answers/{answerId}', name: 'answers_delete', methods: ['DELETE'])]
     public function answers_delete(UserInterface $user, Request $request): JsonResponse
     {
+        dd($request->get('answerId'));
         return $this->json($this->teacherAnswerService->delete($user->getUserIdentifier(),$request->get('answerId')));
     }
     #[Route(path: '/answers/{answerId}', name: 'answers_edit', methods: ['PUT'])]
